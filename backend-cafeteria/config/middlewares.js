a13a316646f34445951b97444f390d4f0bebb577
W3sleyGuilherme <wesleyrego@epfundao.edu.pt>
2026-07-07 10:53:17 +0100

    Fix: ativando CORS para o frontend local e Render

---
+++ backend-cafeteria/config/middlewares.js
@@ -0,0 +1,38 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://cafeteria-api.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://cafeteria-api.onrender.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://seu-frontend.onrender.com', // Coloque a URL do seu frontend aqui
+        'https://*.onrender.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
